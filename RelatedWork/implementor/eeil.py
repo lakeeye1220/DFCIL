726dce621c2706ba53622bbac448921b33044e22
3neutronstar <minsoo.kang0918@gmail.com>
2022-07-02 11:00:39 +0900

    inversion steps

--- RelatedWork/implementor/eeil.py
+++ RelatedWork/implementor/eeil.py
@@ -138,7 +138,7 @@ class EEIL(ICARL):
             ## after train- process exemplar set ##
             if self.configs['natural_inversion']:
                 prefix=os.path.join(self.save_path, self.time_data)
-                inv_images,inv_labels=get_inversion_images(self.model,[0,self.current_num_classes],task_num,epochs=self.configs['inversion_epochs'],prefix=prefix,global_iteration=task_num,bn_reg_scale=3,g_lr=0.001,d_lr=0.0005,a_lr=0.05,var_scale=0.001,l2_coeff=0.00001,num_generate_images=self.configs['memory_size'])
+                inv_images,inv_labels=get_inversion_images(self.model,[self.current_num_classes,self.current_num_classes+self.task_step],task_num,epochs=self.configs['inversion_epochs'],prefix=prefix,global_iteration=task_num,bn_reg_scale=3,g_lr=0.001,d_lr=0.0005,a_lr=0.05,var_scale=0.001,l2_coeff=0.00001,num_generate_images=self.configs['memory_size'])
             else:
                 self.model.eval()
                 print('')
