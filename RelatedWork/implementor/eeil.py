9a1a9c815a73e7ee6e6653f944df2af33c549622
3neutronstar <minsoo.kang0918@gmail.com>
2022-07-03 23:35:34 +0900

    fix eeil bug

--- RelatedWork/implementor/eeil.py
+++ RelatedWork/implementor/eeil.py
@@ -59,6 +59,7 @@ class EEIL(ICARL):
                                     (task_num-1), self.task_step*task_num]
             if (self.configs['natural_inversion'] or self.configs['generative_inversion']) and task_num>1:
                 if 'cifar' in self.configs['dataset']:
+                    inv_images=np.concatenate(inv_images,axis=0) # list (np.array(bsz,3,32,32))
                     inv_images = inv_images.permute(0, 2, 3, 1)
                     self.datasetloader.train_data.data=np.concatenate((self.datasetloader.train_data.data,inv_images),axis=0)
                     self.datasetloader.train_data.targets=np.concatenate((self.datasetloader.train_data.targets,inv_labels),axis=0)
