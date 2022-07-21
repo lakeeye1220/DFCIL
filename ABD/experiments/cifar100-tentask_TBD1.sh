ecf84b7211de17e7b9cd29680624d056942e12f1
3neutronstar <3neutronstar@gmail.com>
2022-07-21 16:44:34 +0900

    script

--- ABD/experiments/cifar100-tentask_TBD1.sh
+++ ABD/experiments/cifar100-tentask_TBD1.sh
@@ -39,9 +39,9 @@ LR=0.1
 python3 -u run_dfcil.py --dataset CIFAR100 --train_aug --rand_split --gpuid $GPUID --repeat $REPEAT \
     --first_split_size $SPLIT --other_split_size $SPLIT --schedule $SCHEDULE --schedule_type decay --batch_size $BS \
     --optimizer $OPT --lr $LR --momentum $MOM --weight_decay $WD \
-    --mu 1e-1 --memory 0 --model_name $MODELNAME --model_type resnet \
+    --mu 100 --memory 0 --model_name $MODELNAME --model_type resnet \
     --learner_type datafree --learner_name TBD1 \
     --gen_model_name CIFAR_GEN --gen_model_type generator \
     --beta 1 --power_iters $PI --deep_inv_params 1e-3 5e1 1e-3 1e3 1 \
     --overwrite $OVERWRITE --max_task $MAXTASK --log_dir ${OUTDIR}/abd \
-    --teacher_type DI
+    --teacher_type DI --gamma 0.4 --p_order 2
