fd6f4e790a218543b25d3032ca18a08226a39d0d
Minsoo Kang <minsoo@jindo.imrc.kist.re.kr>
2022-08-01 11:33:06 +0900

    loader fix

--- AlwaysBeDreaming/dataloaders/loader.py
+++ AlwaysBeDreaming/dataloaders/loader.py
@@ -619,9 +619,9 @@ class iTinyIMNET(iDataset):
 
         from os import path
         #root = self.root
-        root = '../data/'
+        root = '/data/'
         FileNameEnd = 'JPEG'
-        train_dir = path.join(root, 'tiny-imagenet/tiny-imagenet-200/train')
+        train_dir = path.join(root, 'tiny-imagenet/train')
         self.class_names = sorted(os.listdir(train_dir))
         self.names2index = {v: k for k, v in enumerate(self.class_names)}
         self.data = []
@@ -629,7 +629,7 @@ class iTinyIMNET(iDataset):
 
         if self.train:
             for label in self.class_names:
-                d = path.join(root, 'tiny-imagenet/tiny-imagenet-200/train', label)
+                d = path.join(root, 'tiny-imagenet/train', label)
                 for directory, _, names in os.walk(d):
                     for name in names:
                         filename = path.join(directory, name)
@@ -637,7 +637,7 @@ class iTinyIMNET(iDataset):
                             self.data.append(filename)
                             self.targets.append(self.names2index[label])
         else:
-            val_dir = path.join(root, 'tiny-imagenet/tiny-imagenet-200/val')
+            val_dir = path.join(root, 'tiny-imagenet/val_no_split')
             with open(path.join(val_dir, 'val_annotations.txt'), 'r') as f:
                 infos = f.read().strip().split('\n')
                 infos = [info.strip().split('\t')[:2] for info in infos]
