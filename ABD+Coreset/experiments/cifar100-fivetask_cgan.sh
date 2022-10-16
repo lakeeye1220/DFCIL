de8af0002541d636b995479834413606d6431acd
3neutronstar <3neutronstar@naver.com>
2022-10-16 13:44:06 +0000

    conditional gan commit

--- ABD+Coreset/experiments/cifar100-fivetask_cgan.sh
+++ ABD+Coreset/experiments/cifar100-fivetask_cgan.sh
@@ -2,10 +2,10 @@
 
 # process inputs
 DEFAULTGPU=0
-GPUID=0
+GPUID=7
 
 # benchmark settings
-DATE=AAAI2023
+DATE=ISCF_CGAN_latent
 SPLIT=20
 OUTDIR=outputs/${DATE}/DFCIL-fivetask/CIFAR100
 
@@ -36,12 +36,12 @@ LR=0.1
 #########################
 
 # Full Method
-python -u run_dfcil.py --dataset CIFAR100 --train_aug --rand_split --gpuid $GPUID --repeat $REPEAT \
+python3 -u run_dfcil.py --dataset CIFAR100 --train_aug --rand_split --gpuid $GPUID --repeat $REPEAT \
     --first_split_size $SPLIT --other_split_size $SPLIT --schedule $SCHEDULE --schedule_type decay --batch_size $BS \
     --optimizer $OPT --lr $LR --momentum $MOM --weight_decay $WD \
     --mu 1e-1 --memory 0 --model_name $MODELNAME --model_type resnet \
     --learner_type datafree --learner_name ISCF \
     --gen_model_name CIFAR_GEN --gen_model_type generator \
-    --beta 1 --power_iters $PI --deep_inv_params 1e-3 5e1 1e-3 1e3 1 \
+    --beta 1 --power_iters $PI --deep_inv_params 1e-3 5e1 1e-3 1 2 \
     --overwrite $OVERWRITE --max_task $MAXTASK --log_dir ${OUTDIR}/iscf \
     --sp_mu 100 --weq_mu 1 --cgan "$@"
