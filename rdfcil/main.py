823e2b088a8b1c410f114b04e6d1f9a0e9175584
Minsoo Kang <59332148+3neutronstar@users.noreply.github.com>
2023-12-23 15:16:57 +0900

    Add files via upload

---
+++ rdfcil/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from cl_lite.core import App
+
+from datamodule import DataModule
+from iscf_module import ISCFModule
+# from module import Module
+
+
+app = App(
+    ISCFModule,
+    # Module,
+    DataModule,
+    gpus=-1,
+    benchmark=True,
+    num_sanity_val_steps=0,
+    replace_sampler_ddp=False,
+    deterministic=True,
+    checkpoint_callback=False,
+    check_val_every_n_epoch=40,
+    max_epochs=200,
+)
+
+if __name__ == "__main__":
+    app.main()
